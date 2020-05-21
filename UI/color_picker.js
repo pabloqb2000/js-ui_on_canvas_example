25fa3912af2202d7e27b6a04d52c5f71a3172822
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-05-22 00:04:09 +0200

    Added color picker default values

--- UI/color_picker.js
+++ UI/color_picker.js
@@ -13,22 +13,23 @@ class ColorPicker extends UiElement {
      * @param width Width of the sliders
      * @param height Height of the sliders
      * @param margins Margin between the sliders
+     * @param values Default values
      * @param action Action perfomed when color changes
      */
-    constructor(x=20, y=20, width=100, height=10, margins=20, nempty=0, action=null) {
+    constructor(x=20, y=20, width=100, height=10, margins=20, nempty=0, values=null, action=null) {
         super(x,y,width,height, false, false, false);
         this.margins = margins;
 
         let tmp;
-        this.hueSld = new Slider(0, 1, 0.5, x, y, width, height, null, "H", false, 0, action);
+        this.hueSld = new Slider(0, 1, values==null ? 0.5 : values[0], x, y, width, height, null, "H", false, 0, action);
         for(let i = 0; i < nempty; i++)
             tmp = new UiElement();
 
-        this.satSld = new Slider(0, 1, 0.5, x, y + height + margins, width, height, null, "S", false, 0, action);
+        this.satSld = new Slider(0, 1, values==null ? 0.5 : values[1], x, y + height + margins, width, height, null, "S", false, 0, action);
         for(let i = 0; i < nempty; i++)
             tmp = new UiElement();
 
-        this.ligSld = new Slider(0, 1, 0.5, x, y + height*2 + margins*2, width, height, null, "L", false, 0, action);
+        this.ligSld = new Slider(0, 1, values==null ? 0.5 : values[2], x, y + height*2 + margins*2, width, height, null, "L", false, 0, action);
         for(let i = 0; i < nempty; i++)
             tmp = new UiElement();
     }
