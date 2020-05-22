f68df2b9a95491ae81c6e13944a4e46ec39404bb
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-05-23 00:23:20 +0200

    Changed deffault changes again

--- sketch.js
+++ sketch.js
@@ -6,19 +6,26 @@ let cPicker;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(32);
+
+	// Create UI elements
 	sld = new Slider(start=0, end=255, value=32, 0, 0, width/12, height/60, null, "Background");
-	btn = new Button(x=0, y=0, width=100, height=20, "Reset", resetValue);
-	tggl = new ToggleButton(0,0,100,20,"Discrete", discretice);
-	cPicker = new ColorPicker(0,0, 100, 10, 20);
+	btn = new Button(x=0, y=0, width=width/12, height=height/30, "Reset", resetValue);
+	tggl = new ToggleButton(0,0,width/12,height/30,"Discrete", discretice);
+	cPicker = new ColorPicker(0,0, width/12, height/60);
+
+	// Start UI
 	UI.tableWidth = 1;
 	UI.tableHeight = 6;
 	UI.distrubute();
 }
 
 function draw() {
+	// Draw UI
 	background(sld.value);
 	UI.update();
 	UI.draw();
+
+	
 	translate(width/2 + width/12, height/2);
 	scale(1,-1);
 }
